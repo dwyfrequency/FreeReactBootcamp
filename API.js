e59b1032dedf1fda3708ddf6395255a79b4ecedd
dwyfrequency <jdwy215@gmail.com>
2018-11-15 15:46:15 -0500

    adding fake api module and initial lifecycle methods

---
+++ API.js
@@ -0,0 +1,16 @@
+const API = {
+  fetchFriends() {
+    return new Promise((res, rej) => {
+      const friends = [
+        { name: "adam", active: true },
+        { name: "han", active: true },
+        { name: "zach", active: true }
+      ];
+      setTimeout(() => {
+        return res(friends);
+      }, 2000);
+    });
+  }
+};
+
+export default API;
